589e76e388817ce3643433ce578739cf52f90f94
LMaverick <91562063+LMaverick@users.noreply.github.com>
2024-04-07 22:29:10 -0300

    up

--- principal.js
+++ principal.js
@@ -6,3 +6,19 @@ setTimeout(function() {
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('.fake-layer').style.display = 'none'; // Oculta a camada falsa
 }, randomTime);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carrinhoIcon = document.querySelector('.carrinho-icon');
+    const carrinhoTooltip = document.querySelector('.carrinho-tooltip');
+
+    // Evento de clique no ícone do carrinho para exibir/ocultar o tooltip
+    carrinhoIcon.addEventListener('click', function() {
+        if (carrinhoTooltip.style.display === 'block') {
+            carrinhoTooltip.style.display = 'none';
+        } else {
+            carrinhoTooltip.style.display = 'block';
+            // Lógica para abrir o carrinho aqui (ainda não implementada)
+        }
+    });
+});
